f5e89f5764741c39a26daa74d09a184b4923ced9
mahalakshminadigatla <mahalakshminadigatla2001@gmail.com>
2023-11-23 17:42:59 +0530

    Commit 1

---
+++ springboot-restful-webservices/src/main/java/com/example/springbootrestfulwebservices/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.example.springbootrestfulwebservices.controller;
+
+
+import com.example.springbootrestfulwebservices.dto.UserDto;
+import com.example.springbootrestfulwebservices.entity.User;
+import com.example.springbootrestfulwebservices.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private UserService userService;
+
+    //build create user REST API
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user){
+        UserDto savedUSer = userService.createUser(user);
+        return new ResponseEntity<>(savedUSer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId){
+        UserDto user =  userService.getUserById(userId);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        List<UserDto> users = userService.getAllUsers();
+        return new ResponseEntity<>(users,HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId,@RequestBody UserDto user){
+        user.setId(userId);
+        UserDto updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User successfully deleted",HttpStatus.OK);
+    }
+}
---
+++ springboot-restful-webservices/src/main/java/com/example/springbootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.example.springbootrestfulwebservices.service.impl;
+
+import com.example.springbootrestfulwebservices.dto.UserDto;
+import com.example.springbootrestfulwebservices.entity.User;
+import com.example.springbootrestfulwebservices.mapper.UserMapper;
+import com.example.springbootrestfulwebservices.repository.UserRepository;
+import com.example.springbootrestfulwebservices.service.UserService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+
+    private ModelMapper modelMapper;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        //convert UserDto into User JPA entity
+        //User user = UserMapper.mapToUser(userDto);
+        User user = modelMapper.map(userDto,User.class);
+
+        User savedUser = userRepository.save(user);
+
+        //convert User JPA entity into UserDto
+       // UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+
+        UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
+         return savedUserDto;
+    }
+
+    @Override
+    public UserDto getUserById(Long userId) {
+
+       Optional<User> optionalUser = userRepository.findById(userId);
+        User user = optionalUser.get();
+//        return UserMapper.mapToUserDto(user);
+
+        return modelMapper.map(user,UserDto.class);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+      //  return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+
+        return users.stream().map((user) -> modelMapper.map(user,UserDto.class) ).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto updateUser(UserDto user) {
+        User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        User updatedUser = userRepository.save(existingUser);
+      //  return UserMapper.mapToUserDto(updatedUser);
+
+        return modelMapper.map(updatedUser,UserDto.class);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+
+}
